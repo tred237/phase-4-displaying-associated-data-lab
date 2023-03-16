0cd3ab7d81850b054ce6c94be21e50a29203257e
trevor dewindt <trevor.dewindt@gmail.com>
2023-03-15 20:21:12 -0600

    commit tree af75a1efbac795bfc2a723dd54969283dca858e8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :user_not_found_error_response
+
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+
+    def user_not_found_error_response invalid
+        render json: {error: "User not found"}, status: :not_found
+    end
 end
